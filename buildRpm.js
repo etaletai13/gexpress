c40968568ea96131f4676fe92c0ce8e1d1fb4a48
Travis CI <travis@travis-ci.org>
2018-07-11 14:17:26 -0700

    --amend

---
+++ buildRpm.js
@@ -0,0 +1,22 @@
+var buildRpm = require('rpm-builder');
+
+var options = {
+  name: 'GExpress',
+  version: '1.0.0',
+  release: '1',
+  buildArch: 'x86_64',
+  summary: 'Google Express client',
+  rpmDest: './Build/Release/',
+  verbose: true,
+  files: [
+    {src: './g-express-linux-x64/', dest: '/dist/'},
+  ]
+};
+
+buildRpm(options, function(err, rpm) {
+  if (err) {
+    throw err;
+  }
+
+  console.log(rpm);
+});
